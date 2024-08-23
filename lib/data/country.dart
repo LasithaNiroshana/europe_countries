b2e6be2e1325c5a8b554d973cd4ee303b3bad275
Lasitha Niroshana <lasithaniroshanawm@gmail.com>
2024-08-23 13:04:57 +0530

    Modified country model

--- lib/data/country.dart
+++ lib/data/country.dart
@@ -3,14 +3,12 @@ import 'package:json_annotation/json_annotation.dart';
 part 'country.g.dart';
 
 @JsonSerializable()
-//Country model
-class Country{
-
+class Country {
   final Name name;
   final List<String> capital;
   final Flags flags;
   final String region;
-  final Map<String,String>languages;
+  final Map<String, String> languages;
   final int population;
 
   Country({
@@ -19,38 +17,35 @@ class Country{
     required this.flags,
     required this.region,
     required this.languages,
-    required this.population
+    required this.population,
   });
 
-   factory Country.fromJson(Map<String, dynamic> json) => _$CountryFromJson(json);
+  factory Country.fromJson(Map<String, dynamic> json) => _$CountryFromJson(json);
   Map<String, dynamic> toJson() => _$CountryToJson(this);
-
 }
 
 @JsonSerializable()
-//Name model
-class Name{
+class Name {
   final String commonName;
   final String officialName;
 
   Name({
     required this.commonName,
-    required this.officialName
+    required this.officialName,
   });
 
   factory Name.fromJson(Map<String, dynamic> json) => _$NameFromJson(json);
   Map<String, dynamic> toJson() => _$NameToJson(this);
 }
 
- @JsonSerializable()
- //Flags model
-class Flags{
+@JsonSerializable()
+class Flags {
   final String pngImage;
   final String svgImage;
 
   Flags({
     required this.pngImage,
-    required this.svgImage
+    required this.svgImage,
   });
 
   factory Flags.fromJson(Map<String, dynamic> json) => _$FlagsFromJson(json);
--- lib/data/country.g.dart
+++ lib/data/country.g.dart
@@ -6,15 +6,22 @@ part of 'country.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-Country _$CountryFromJson(Map<String, dynamic> json) => Country(
-      name: Name.fromJson(json['name'] as Map<String, dynamic>),
-      capital:
-          (json['capital'] as List<dynamic>).map((e) => e as String).toList(),
-      flags: Flags.fromJson(json['flags'] as Map<String, dynamic>),
-      region: json['region'] as String,
-      languages: Map<String, String>.from(json['languages'] as Map),
-      population: (json['population'] as num).toInt(),
-    );
+Country _$CountryFromJson(Map<String, dynamic> json) {
+  return Country(
+    name: Name.fromJson(json['name'] as Map<String, dynamic>),
+    capital: (json['capital'] as List<dynamic>?)
+            ?.map((e) => e as String)
+            .toList() ??
+        [], 
+    flags: Flags.fromJson(json['flags'] as Map<String, dynamic>),
+    region: json['region'] as String? ?? 'Unknown', 
+    languages: (json['languages'] as Map<String, dynamic>?)
+            ?.map((k, e) => MapEntry(k, e as String)) ??
+        {}, 
+    population: json['population'] as int? ?? 0, 
+  );
+}
+
 
 Map<String, dynamic> _$CountryToJson(Country instance) => <String, dynamic>{
       'name': instance.name,
@@ -25,20 +32,24 @@ Map<String, dynamic> _$CountryToJson(Country instance) => <String, dynamic>{
       'population': instance.population,
     };
 
-Name _$NameFromJson(Map<String, dynamic> json) => Name(
-      commonName: json['commonName'] as String,
-      officialName: json['officialName'] as String,
-    );
+Name _$NameFromJson(Map<String, dynamic> json) {
+  return Name(
+    commonName: json['common'] as String? ?? 'Unknown', 
+    officialName: json['official'] as String? ?? 'Unknown',
+  );
+}
 
 Map<String, dynamic> _$NameToJson(Name instance) => <String, dynamic>{
       'commonName': instance.commonName,
       'officialName': instance.officialName,
     };
 
-Flags _$FlagsFromJson(Map<String, dynamic> json) => Flags(
-      pngImage: json['pngImage'] as String,
-      svgImage: json['svgImage'] as String,
-    );
+Flags _$FlagsFromJson(Map<String, dynamic> json) {
+  return Flags(
+    pngImage: json['png'] as String? ?? '', 
+    svgImage: json['svg'] as String? ?? '', 
+  );
+}
 
 Map<String, dynamic> _$FlagsToJson(Flags instance) => <String, dynamic>{
       'pngImage': instance.pngImage,
